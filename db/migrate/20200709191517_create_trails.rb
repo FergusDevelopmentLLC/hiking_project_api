3f4d30c1a60bafaed9c31f4bf2aae3e5d8c8323f
Will Carter <will.carter@fergusllc.com>
2020-07-09 13:17:47 -0600

    added rails api and trail model

---
+++ db/migrate/20200709191517_create_trails.rb
@@ -0,0 +1,33 @@
+class CreateTrails < ActiveRecord::Migration[6.0]
+  def change
+    create_table :trails do |t|
+      t.string :name
+      t.string :trail_type
+      t.string :summary
+      t.string :difficulty
+      t.float :stars
+      t.integer :starVotes
+      t.string :location
+      t.string :url
+      t.string :imgSqSmall
+      t.string :imgSmall
+      t.string :imgSmallMed
+      t.string :imgMedium
+      t.float :length
+      t.integer :ascent
+      t.integer :descent
+      t.integer :high
+      t.integer :low
+      t.float :longitude
+      t.float :latitude
+      t.string :conditionStatus
+      t.string :conditionDetails
+      t.date :conditionDate
+      t.string :features
+      t.string :overview
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,45 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_07_09_191517) do
+
+  create_table "trails", force: :cascade do |t|
+    t.string "name"
+    t.string "trail_type"
+    t.string "summary"
+    t.string "difficulty"
+    t.float "stars"
+    t.integer "starVotes"
+    t.string "location"
+    t.string "url"
+    t.string "imgSqSmall"
+    t.string "imgSmall"
+    t.string "imgSmallMed"
+    t.string "imgMedium"
+    t.float "length"
+    t.integer "ascent"
+    t.integer "descent"
+    t.integer "high"
+    t.integer "low"
+    t.float "longitude"
+    t.float "latitude"
+    t.string "conditionStatus"
+    t.string "conditionDetails"
+    t.date "conditionDate"
+    t.string "features"
+    t.string "overview"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
