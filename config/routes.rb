60649b87102994eeabb95371c364a6d6c268b1e8
Will Carter <will.carter@fergusllc.com>
2020-07-10 12:48:11 -0600

    add max_distance and max_results params to for_coords route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
-  # this goes to the api
-  get 'trails/:latitude/:longitude', to: 'trails#for_coords', constraints: { :latitude => /[^\/]+/, :longitude => /[^\/]+/ }
-
   resources :trails
+
+  # this goes to the api
+  get 'trails/:latitude/:longitude/:max_distance/:max_results', to: 'trails#for_coords', constraints: { 
+    :latitude => /[^\/]+/, 
+    :longitude => /[^\/]+/,
+    :max_distance => /[^\/]+/,
+    :max_results => /[^\/]+/,
+  }
+
 end
