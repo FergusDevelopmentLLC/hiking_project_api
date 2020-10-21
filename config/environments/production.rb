26cc0d5f6773647c00102d0a1549348be1fd5143
Will Carter <will.carter@fergusllc.com>
2020-10-21 13:19:37 -0600

    force ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,4 +103,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.require_master_key = true
+
+  config.force_ssl = true
+
 end
